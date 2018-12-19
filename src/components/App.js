6e92334fb8865bbbfcadc7c09ab6e24c3bcef198
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-19 12:48:46 -0600

    added history to browserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,13 @@ import StreamDelete from './streams/StreamDelete'
 import StreamList from './streams/StreamList'
 import StreamShow from './streams/StreamShow'
 import Header from './Header'
+import history from '../history'
 
 const App = () => {
   return (
     <div className='ui container '>
       <Header />
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <div>
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' component={StreamCreate} />
