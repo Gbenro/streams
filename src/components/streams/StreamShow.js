6af65cdb69aa79a2d85a95435239a712dc634a3e
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-20 16:58:03 -0600

    wired up live stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -9,6 +9,22 @@ class StreamShow extends React.Component {
   }
   componentDidMount () {
     this.props.fetchStream(this.props.match.params.id)
+    this.buildPlayer()
+  }
+  componentDidUpdate () {
+    this.buildPlayer()
+  }
+  buildPlayer () {
+    if (this.player || !this.props.stream) {
+      return
+    }
+    const { id } = this.props.match.params
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}`
+    })
+    this.player.attachMediaElement(this.videoRef.current)
+    this.player.load()
   }
   render () {
     if (!this.props.stream) {
